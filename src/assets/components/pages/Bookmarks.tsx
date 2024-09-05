632375e03cf2fb14da526fcd6a608977caf9d3a6
Christiannjhay <sugarolchristian8@gmail.com>
2024-09-05 16:24:17 +0800

    updaetd loading

--- src/assets/components/pages/Bookmarks.tsx
+++ src/assets/components/pages/Bookmarks.tsx
@@ -55,25 +55,26 @@ export default function Bookmarks() {
 
   if (loading) {
     return (
-      <p className="text-center text-white justify-center items-center">
-        <button type="button" className=" ..." disabled>
-          <svg className="animate-spin h-10 w-10 mr-3 ..." viewBox="0 0 24 24">
-            <circle
-              className="opacity-25"
-              cx="12"
-              cy="12"
-              r="10"
-              stroke="currentColor"
-              strokeWidth="4"
-            ></circle>
-            <path
-              className="opacity-75"
-              fill="currentColor"
-              d="M4 12a8 8 0 018-8V2.5"
-            ></path>
-          </svg>
-        </button>
-      </p>
+      <div className="flex justify-center items-center h-screen bg-[#181818]">
+        <svg
+          className="animate-spin h-10 w-10 text-white"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            className="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            strokeWidth="4"
+          ></circle>
+          <path
+            className="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V2.5"
+          ></path>
+        </svg>
+      </div>
     );
   }
 
