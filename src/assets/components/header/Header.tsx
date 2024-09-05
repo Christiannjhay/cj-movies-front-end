5aaafd570a07f6c289e2a3347c9073cda1dfc790
Christiannjhay <sugarolchristian8@gmail.com>
2024-09-05 15:45:09 +0800

    updated header style

--- src/assets/components/header/Header.tsx
+++ src/assets/components/header/Header.tsx
@@ -12,13 +12,17 @@ export default function Header() {
   return (
     <div className="w-100% h-[150px] grid grid-cols-1 bg-black sm:grid-cols-12 sm:h-[100px] ">
       <div className="flex col-span-1 justify-center items-center sm:col-span-3">
-        <Link to="/">
-          <Logo />
-        </Link>
-      </div>
-      <div className="col-span-1">
-        <div className="sm:hidden flex items-center justify-end mt-5">
-          <SheetSide />
+        <div className="grid grid-cols-12">
+          <div className="col-span-11">
+            <Link to="/">
+              <Logo />
+            </Link>
+          </div>
+          <div className="col-span-1">
+            <div className="sm:hidden flex items-center justify-end mt-5">
+              <SheetSide />
+            </div>
+          </div>
         </div>
       </div>
       <div className="flex items-center mb-4 justify-center col-span-6 sm:mb-0">
