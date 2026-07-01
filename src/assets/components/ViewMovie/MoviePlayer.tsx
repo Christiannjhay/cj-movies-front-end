653012871b15db9fc045b96c62def53e5e8c85a2
christiankonscious <csugarol@konsciousketo.com>
2026-07-01 10:39:36 +0800

    updated vidscr url

--- src/assets/components/ViewMovie/MoviePlayer.tsx
+++ src/assets/components/ViewMovie/MoviePlayer.tsx
@@ -70,7 +70,7 @@ const MoviePlayer: React.FC<MoviePlayerProps> = ({ setMovieDetails }) => {
           className="mt-6 h-[90%] w-11/12"
           width="70%"
           height="700"
-          src={`https://vidsrc.net/embed/${id}`}
+          src={`https://vidsrc.to/embed/${id}`}
           allow="autoplay; fullscreen; picture-in-picture"
           allowFullScreen
           title="Movie"
